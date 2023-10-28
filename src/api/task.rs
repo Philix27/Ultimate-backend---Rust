713d815a5c804a05ae48ad8359d2f21b526c8da3
Philix27 <philixbob@gmail.com>
2023-10-28 13:27:42 +0100

    first commit

---
+++ src/api/task.rs
@@ -0,0 +1,24 @@
+// #[allow(dead_code)]
+use actix_web::{
+    error::ResponseError,
+    get,
+    http::{header::ContentType, StatusCode},
+    middleware::Logger,
+    post, put,
+    web::Data,
+    web::Json,
+    web::Path,
+    HttpResponse,
+};
+use serde::{Deserialize, Serialize};
+
+// use derive_more::{Display};
+#[derive(Deserialize, Serialize)]
+pub struct TaskIdentifier {
+    task_global_id: String,
+}
+
+#[get("/task/{task_global_id}")]
+pub async fn get_task(task_identifier: Path<TaskIdentifier>, body: Json<Struct>) -> Json<String> {
+    Json("Hello, from get task".to_owned()) 
+}
