23efca19aa9b109700378e233c93f15753f14c68
Philix27 <philixbob@gmail.com>
2023-10-30 23:57:34 +0100

    adding more routes

--- src/api/task.rs
+++ src/api/task.rs
@@ -1,14 +1,16 @@
-// #[allow(dead_code)]
+use std::{iter::once, error::Error};
+
+#[allow(dead_code)]
 use actix_web::{
     error::ResponseError,
     get,
     http::{header::ContentType, StatusCode},
     middleware::Logger,
     post, put,
-    web::Data,
+    web::{Data, self},
     web::Json,
     web::Path,
-    HttpResponse,
+    HttpRequest, HttpResponse, HttpResponseBuilder,
 };
 use serde::{Deserialize, Serialize};
 
@@ -22,3 +24,4 @@ pub struct TaskIdentifier {
 pub async fn get_task(task_identifier: Path<TaskIdentifier>, body: Json<String>) -> Json<String> {
     Json("Hello, from get task".to_owned())
 }
+
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,34 @@
 mod api;
 use api::task::get_task;
 
-use actix_web::{middleware::Logger, web::Data, App, HttpServer};
+use actix_web::{
+    middleware::Logger,
+    web::{self, Data},
+    App, HttpResponse, HttpServer, Responder,
+};
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     std::env::set_var("RUST_LOG", "debug");
     std::env::set_var("RUST_BACKTRACE", "1");
     // env_logger::init();
 
-    HttpServer::new(move || App::new().wrap(Logger::default()).service(get_task))
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+    HttpServer::new(move || {
+        App::new()
+            .wrap(Logger::default())
+            .service(web::resource("/").to(index))
+            .service(web::resource("/users/{username}").to(user_info))
+            .service(get_task)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
+
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello, World!")
+}
+
+async fn user_info(path: web::Path<(String,)>) -> impl Responder {
+    let username = &path.0;
+    HttpResponse::Ok().body(format!("User info for: {}", username))
 }
